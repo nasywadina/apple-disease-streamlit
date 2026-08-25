8f50c8805162229a85986fcdb97fbabaae83470e
nasywadina <156357930+nasywadina@users.noreply.github.com>
2026-08-26 03:33:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,12 +169,12 @@ st.markdown(
     /* ---------- FOOTER ---------- */
     .footer{padding:36px 0; text-align:center; color:var(--stone); font-size:0.85rem; border-top:1px solid var(--line);}
 
-    /* 1. Input Gambar Buah Apel", dst → hitam */
+    /* Input Gambar Buah Apel", dst → hitam */
     h4{color:#000000 !important;}
 
     /* "Upload Gambar (JPG/PNG)" & "Real-time Kamera (Webcam)" */
     button[data-baseweb="tab"]{opacity:1 !important;}
-    button[data-baseweb="tab"] p{color:var(--charcoal) !important; opacity:1 !important;}
+    button[data-baseweb="tab"] p{color:var(--stone) !important; opacity:1 !important;}
     button[data-baseweb="tab"][aria-selected="true"] p{color:var(--apple-red) !important;}
     div[data-baseweb="tab-highlight"]{background-color:var(--apple-red) !important;}
     div[data-baseweb="tab-border"]{background-color:var(--line) !important;}
